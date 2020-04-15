5a9dea79ce418e89e8a7d1cbce4a56aebd1f5d63
liwenbo <lwb@isspu.com>
2020-04-15 16:34:01 +0800

    开始自己的小程序，还没想好做什么（手动狗头）

---
+++ miniprogram/pages/my/my.js
@@ -0,0 +1,21 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+  }
+})
